0d7435739657315839f7aa22ed6215972742a7b4
George Bobrov <skivvvvy@gmail.com>
2021-02-07 00:01:58 +0200

    Save settings for each playlist (instead of common to all)

--- PauseOnEnd.js
+++ PauseOnEnd.js
@@ -6,84 +6,93 @@ addObserver();
 
 
 function checkAndCreate() {
-  var containerToPlace = document.querySelector(selectorContainerToPlace);  
-  if (! containerToPlace) 
-    console.log('No container to place PauseOnEnd')
-  else { 
-    var alreadyCreated = document.getElementById(idPauseOnEnd);
-    if (! alreadyCreated) {
-      containerToPlace.appendChild(createCheckboxElement())
-    }
-  }  
+	var containerToPlace = document.querySelector(selectorContainerToPlace);  
+	if (! containerToPlace) 
+		console.log('No container to place PauseOnEnd')
+	else { 
+		var alreadyCreated = document.getElementById(idPauseOnEnd);
+		if (! alreadyCreated) {
+			containerToPlace.appendChild(createCheckboxElement())
+		}
+	}  
 }
 
 function createCheckboxElement(){
-  console.log('Create PauseOnEnd element on ' + document.URL); 
-  var div = document.createElement('div'); 
-  div.id = idPauseOnEnd;
-  div.style.marginTop = '13px';
-  div.style.marginLeft = '20px';
-  div.style.fontSize = '12px';
+	console.log('Create PauseOnEnd element on ' + document.URL); 
+	var div = document.createElement('div'); 
+	div.id = idPauseOnEnd;
+	div.style.marginTop = '13px';
+	div.style.marginLeft = '20px';
+	div.style.fontSize = '12px';
 
-  var checkboxPauseOnEnd = document.createElement('input');
-  checkboxPauseOnEnd.type = "checkbox";  
-  checkboxPauseOnEnd.id = idcheckboxPauseOnEnd;
-  checkboxPauseOnEnd.style.margin = '-3px 1px 0px 2px';
-  checkboxPauseOnEnd.style.verticalAlign = 'middle';
-  checkboxPauseOnEnd.onclick = cbPauseOnEndOnClick;
-  readSettings(checkboxPauseOnEnd);
+	var checkboxPauseOnEnd = document.createElement('input');
+	checkboxPauseOnEnd.type = "checkbox";  
+	checkboxPauseOnEnd.id = idcheckboxPauseOnEnd;
+	checkboxPauseOnEnd.style.margin = '-3px 1px 0px 2px';
+	checkboxPauseOnEnd.style.verticalAlign = 'middle';
+	checkboxPauseOnEnd.onclick = cbPauseOnEndOnClick;
+	readSettings(checkboxPauseOnEnd);
 
-  var span = document.createElement('span');  
-  span.innerHTML = 'Pause On End';
-  span.style.marginLeft = '5px';
-  span.style.color = '#767676';
+	var span = document.createElement('span');  
+	span.innerHTML = 'Pause On End';
+	span.style.marginLeft = '5px';
+	span.style.color = '#767676';
 
-  div.appendChild(checkboxPauseOnEnd);
-  div.appendChild(span);
-  return div;
+	div.appendChild(checkboxPauseOnEnd);
+	div.appendChild(span);
+	return div;
 }
-   
+	 
 
 function readSettings(checkbox) {
-  chrome.storage.local.get([idPauseOnEnd], function(data) {
-    checkbox.checked = data[idPauseOnEnd];
-  });
+	let playlistID = getPlaylistID();
+	if (!playlistID) return;
+
+	chrome.storage.local.get([playlistID], function(data) {
+		checkbox.checked = data[playlistID] || false;
+	});
 }
 
 var dontPauseAgain = false; 
 
 function addObserver(){
 
-  setInterval(function() {
-    let checkboxPauseOnEnd = document.getElementById(idcheckboxPauseOnEnd)
-    if (! checkboxPauseOnEnd) 
-      checkAndCreate();
-      
-    checkboxPauseOnEnd = document.getElementById(idcheckboxPauseOnEnd)
-    let videos = document.querySelectorAll('video')
+	setInterval(function() {
+		let checkboxPauseOnEnd = document.getElementById(idcheckboxPauseOnEnd)
+		if (! checkboxPauseOnEnd) 
+			checkAndCreate();
+			
+		checkboxPauseOnEnd = document.getElementById(idcheckboxPauseOnEnd)
+		let videos = document.querySelectorAll('video')
 
-    if ((videos.length > 0) && (checkboxPauseOnEnd) && (checkboxPauseOnEnd.checked) ) {
-      if (videos[0].duration - videos[0].currentTime < 1) {
-        if (! dontPauseAgain) { 
-          videos[0].pause();
-          //Pause on the end of video only one time
-          dontPauseAgain = true; //if the user clicks "play", the pause will not occur again
-        }
-      }
-      else
-        dontPauseAgain = false; //if the video was rewound, can pause again on the end
-    }
-    
-  }, 1000)
+		if ((videos.length > 0) && (checkboxPauseOnEnd) && (checkboxPauseOnEnd.checked) ) {
+			if (videos[0].duration - videos[0].currentTime < 1) {
+				if (! dontPauseAgain) { 
+					videos[0].pause();
+					//Pause on the end of video only one time
+					dontPauseAgain = true; //if the user clicks "play", the pause will not occur again
+				}
+			}
+			else
+				dontPauseAgain = false; //if the video was rewound, can pause again on the end
+		}
+		
+	}, 1000)
 
 }
 
 
 function cbPauseOnEndOnClick(event) {
-  let checked = event.target.checked
+	let checked = event.target.checked
+	let playlistID = getPlaylistID();
+	if (!playlistID) return;
 
-  chrome.storage.local.set({ [idPauseOnEnd]: checked }, function() {
-    console.info("Saved PauseOnEnd " + checked);
-  });  
+	chrome.storage.local.set({ [playlistID]: checked }, function() {
+		console.info(`Saved PauseOnEnd = ${checked} on playlist ${playlistID}`);
+	});
+
 }
 
+function getPlaylistID() {
+	return document.URL.match(/list=([0-9a-zA-Z-]+)/)[1];
+}	
