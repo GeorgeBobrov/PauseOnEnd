fdadd66481cbb0b39931f4de619681d7887b4b8d
George Bobrov <skivvvvy@gmail.com>
2021-06-24 18:53:19 +0300

    Deleted observer. Changed #top-level-buttons to #top-level-buttons-computed (Youtube internal changes)

--- PauseOnEnd.js
+++ PauseOnEnd.js
@@ -1,39 +1,40 @@
+/*global chrome*/
 var idPauseOnEnd = 'PauseOnEnd';
 var idcheckboxPauseOnEnd = 'cbPauseOnEnd';
-var selectorContainerToPlace = '#playlist-actions #top-level-buttons'
+var selectorContainerToPlace = '#playlist-actions #top-level-buttons-computed';
 
 // console.log(new Date().toISOString() + ' run PauseOnEnd.js on ' + document.URL);
 
 var observerId;
-if (!observerId) 
+if (!observerId)
 	observerId = addObserver();
 
 
 function checkContainerAndCreate() {
-	let containerToPlace = document.querySelector(selectorContainerToPlace);  
-	if (!containerToPlace) 
+	let containerToPlace = document.querySelector(selectorContainerToPlace);
+	if (!containerToPlace)
 		console.log('No container to place PauseOnEnd')
-	else  
+	else
 		containerToPlace.appendChild(createPauseOnEndElement())
 }
 
 function createPauseOnEndElement(){
-	console.log(new Date().toISOString() +' Create PauseOnEnd element on ' + document.URL); 
-	let div = document.createElement('div'); 
+	console.log(new Date().toISOString() +' Create PauseOnEnd element on ' + document.URL);
+	let div = document.createElement('div');
 	div.id = idPauseOnEnd;
 	div.style.marginTop = '13px';
 	div.style.marginLeft = '20px';
 	div.style.fontSize = '12px';
 
 	let checkboxPauseOnEnd = document.createElement('input');
-	checkboxPauseOnEnd.type = "checkbox";  
+	checkboxPauseOnEnd.type = "checkbox";
 	checkboxPauseOnEnd.id = idcheckboxPauseOnEnd;
 	checkboxPauseOnEnd.style.margin = '-3px 1px 0px 2px';
 	checkboxPauseOnEnd.style.verticalAlign = 'middle';
 	checkboxPauseOnEnd.onclick = cbPauseOnEndOnClick;
 	readSettings(checkboxPauseOnEnd);
 
-	let span = document.createElement('span');  
+	let span = document.createElement('span');
 	span.innerHTML = 'Pause On End';
 	span.style.marginLeft = '5px';
 	span.style.color = '#767676';
@@ -42,7 +43,7 @@ function createPauseOnEndElement(){
 	div.appendChild(span);
 	return div;
 }
-	 
+
 
 function readSettings(checkbox) {
 	let playlistID = getPlaylistID();
@@ -53,54 +54,24 @@ function readSettings(checkbox) {
 	});
 }
 
-var dontPauseAgain = false; 
+var dontPauseAgain = false;
 
 function addObserver(){
-	let playlistActionsConteiner = document.querySelector('#playlist-actions')
-
-	if (playlistActionsConteiner) {
-		let observer = new MutationObserver(mutationRecords => {
-			for (const mutationRecord of mutationRecords) {
-				// console.log(mutationRecord.target);
-				// if (mutationRecord.removedNodes.length > 0)
-				//   console.log(mutationRecord.removedNodes[0].nodeName);
-				// for (const removedNode of mutationRecord.removedNodes) 
-				// 	console.log(new Date().toISOString() + ' removedNode: ' + removedNode);
-
-				// for (const addedNode of mutationRecord.addedNodes) 
-				// 	console.log(new Date().toISOString() + ' addedNode: ' + addedNode);
-
-				if ((mutationRecord.type == "childList") &&
-					(mutationRecord.removedNodes.length > 0) && 
-					(mutationRecord.removedNodes[0].id == idPauseOnEnd))
-					{
-						console.log(new Date().toISOString() + ' mutation: PauseOnEnd deleted ');
-						checkContainerAndCreate();
-					}
-			}
-		});
-
-		observer.observe(playlistActionsConteiner, {
-			childList: true, 
-			subtree: true,
-			characterData: false
-		}); 
-	}
 
 	return setInterval(function() {
 		//some parts of youtube interface can be hidden, exclude them from the search
-		let checkboxPauseOnEnd = 
+		let checkboxPauseOnEnd =
 			document.querySelector(`ytd-playlist-panel-renderer:not([hidden]) #${idcheckboxPauseOnEnd}`);
 		if (!checkboxPauseOnEnd) {
 			checkContainerAndCreate();
 			checkboxPauseOnEnd = document.getElementById(idcheckboxPauseOnEnd)
-		} 
-			
+		}
+
 		let video = document.querySelector('video')
 
 		if ((video) && (checkboxPauseOnEnd) && (checkboxPauseOnEnd.checked) ) {
 			if (video.duration - video.currentTime < video.playbackRate) {
-				if (!dontPauseAgain) { 
+				if (!dontPauseAgain) {
 					video.pause();
 					//Pause on the end of video only one time
 					dontPauseAgain = true; //if the user clicks "play", the pause will not occur again
@@ -109,7 +80,7 @@ function addObserver(){
 			else
 				dontPauseAgain = false; //if the video was rewound, can pause again on the end
 		}
-		
+
 	}, 1000)
 
 }
