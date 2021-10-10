e301c3227abdd7452ade86846284dc82abca1679
George Bobrov <skivvvvy@gmail.com>
2021-10-10 04:25:17 +0300

    Сhanged the launch logic only on playlists

--- background.js
+++ background.js
@@ -4,8 +4,8 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
     // console.log(tab);
     if (changeInfo.title)
     if (tab.status === "complete")
-    if (tab.url.startsWith("https://www.youtube.com/")) {
-        if (tab.url.includes("index=")) {
+    if (tab.url.startsWith("https://www.youtube.com/watch")) {
+        if (tab.url.includes("list=")) {
             console.log('run PauseOnEnd.js on ' + tab.url);
             chrome.tabs.executeScript(tabId, {file:"PauseOnEnd.js"});
         }
